80d7c66df9e5eec9e906e7ebaf2d97e554d379c2
leanseller <danil.d776@gmail.com>
2026-04-27 16:31:42 +0500

    [refactor] Убрано все ненужное

--- core/utils/config.py
+++ core/utils/config.py
@@ -1,5 +1,4 @@
 import simplejson as json
-from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from core.models import UploadFilesPageParams
 from core.models.base import (
@@ -10,7 +9,6 @@ from core.models.base import (
 )
 from core.models.setup import SetupParams
 from core.models.statement import StatementFillInfoParams
-from settings import CONFIGS, PATH
 
 
 def read_config(config_name: str) -> SetupParams:
@@ -20,25 +18,25 @@ def read_config(config_name: str) -> SetupParams:
         raise TypeError("Config must be a JSON file")
 
     with open(config_name, "r", encoding="utf-8") as config_file:
-        config_name = json.load(config_file)
+        config_file = json.load(config_file)
 
     auth_params = AuthParams(
-        bin=config_name["auth"]["creds"]["bin"],
-        password=config_name["auth"]["creds"]["password"],
-        nca_path=config_name["auth"]["nca"]["nca_path"],
-        nca_password=config_name["auth"]["nca"]["nca_password"],
+        bin=config_file["auth"]["creds"]["bin"],
+        password=config_file["auth"]["creds"]["password"],
+        nca_path=config_file["auth"]["nca"]["nca_path"],
+        nca_password=config_file["auth"]["nca"]["nca_password"],
     )
 
-    if config_name.get("type") == "statement":
-        fill_info_params = StatementFillInfoParams(**config_name["fill_info_page"])
+    if config_file.get("type") == "statement":
+        fill_info_params = StatementFillInfoParams(**config_file["fill_info_page"])
     else:
-        fill_info_params = FillInfoPageParams(**config_name["fill_info_page"])
+        fill_info_params = FillInfoPageParams(**config_file["fill_info_page"])
 
     select_options_params = SelectOptionsPageParams(
-        **config_name["select_options_page"]
+        **config_file["select_options_page"]
     )
-    payment_params = PaymentPageParams(**config_name["payment_page"])
-    upload_files_params = UploadFilesPageParams(**config_name["upload_files_page"])
+    payment_params = PaymentPageParams(**config_file["payment_page"])
+    upload_files_params = UploadFilesPageParams(**config_file["upload_files_page"])
 
     return SetupParams(
         auth_params=auth_params,
@@ -46,20 +44,8 @@ def read_config(config_name: str) -> SetupParams:
         fill_info_page_params=fill_info_params,
         payment_page_params=payment_params,
         upload_files_page_params=upload_files_params,
-        type=config_name["type"],
-        db_scheme=config_name["db_scheme"],
-        result_dir_path=str(PATH / config_name["result_dir_path"]),
-        max_cases_per_day=config_name["max_cases_per_day"],
+        type=config_file["type"],
+        db_scheme=config_file["db_scheme"],
+        result_dir_path=config_file["result_dir_path"],
+        max_cases_per_day=config_file["max_cases_per_day"],
     )
-
-
-class Settings(BaseSettings):
-    supabase_url: str
-    supabase_key: str
-
-    config: SetupParams = read_config(str(CONFIGS / "ccloan.json"))
-
-    model_config = SettingsConfigDict(env_file=PATH / ".env")
-
-
-settings = Settings()  # type: ignore
